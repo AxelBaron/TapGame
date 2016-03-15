4a16a16d8188bc9b1bfe5820196493cbe18799e5
AxelBaron <axelbaron@outlook.fr>
2016-03-15 09:09:37 +0100

    Js
    …

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,33 @@
-document.addEventListener("deviceready", onDeviceReady, false);
+$(document).ready(function(){
+  console.log("go!!");
+  $("#btn_go").click(function(){
 
-function onDeviceReady() {
-  console.log('test');
- }
+    //CSS pour changer d'écran.
+    $('#startScreen').css('display','none');
+    $('.app').css('background','none');
+
+    //Scokage des données.
+    var pseudo = $('#pseudo').val();
+
+    //CSS nouvel écran
+    $('#game').css('display','block');
+
+    var compter = 5;
+    var stop='';
+    setInterval(function(){
+      if (stop=='') {
+        compter = compter - 1;
+        $('#timer').html(compter);
+        if (compter <= 0) {
+          startGame();
+          stop = 'stop';
+        }
+      }
+    }, 1000);
+  });
+});
+
+function startGame(){
+  alert('startGame');
+  $('#timer').text('GO !');
+}
